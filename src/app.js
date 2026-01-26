3a1d16bdb4e83cd8864a17e004930432bda62c87
iDanielSoto <dansoto804@gmail.com>
2026-01-25 22:47:37 -0600

    q

--- src/app.js
+++ src/app.js
@@ -26,13 +26,16 @@ const app = express();
 
 app.set('trust proxy', true);
 app.use(cors({
-    origin: "*",
+    origin: [
+        '*',
+        'app://'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
